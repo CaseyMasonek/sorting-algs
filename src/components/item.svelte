351c8da0f503c7e0ec071bfff297f2e060fe3146
Casey Masonek <cmmasonek@gmail.com>
2025-09-08 12:00:12 -0700

    Change color of "special" items to make it easier to see

--- src/components/item.svelte
+++ src/components/item.svelte
@@ -2,6 +2,6 @@
     let { value, order, selected, special } = $props()
 </script>
 
-<div class={["w-10 h-10 border-2 rounded-md text-center align-middle", order[value].value  == value && "bg-green-300", selected && "border-yellow-300", special && "border-red-500"]}>
+<div class={["w-10 h-10 border-2 rounded-md text-center align-middle", order[value].value  == value && "bg-green-300", selected && "border-yellow-300", special && "border-blue-200"]}>
     {value}
 </div>
--- src/components/littleItem.svelte
+++ src/components/littleItem.svelte
@@ -2,5 +2,5 @@
     let { value, order, selected, special } = $props()
 </script>
 
-<div class={[`w-full border-1 rounded-md`, order[value].value  == value && "bg-green-300", selected && "border-yellow-300", special && "border-red-500"]} style={`height:${value * 100/order.length}%`}>
+<div class={[`w-full border-1 rounded-md`, order[value].value  == value && "bg-green-300", selected && "border-yellow-300", special && "border-blue-200"]} style={`height:${value * 100/order.length}%`}>
 </div>
